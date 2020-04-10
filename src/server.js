eb4cf581edea7f722f4ac7bd4d65713670ea6752
ricardozv <ricardosilvanet03@gmail.com>
2020-04-10 17:16:27 -0300

    up server.js, insert mongoose in server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
+
+
+mongoose.connect('mongodb+srv://ricozv:drexy1983@mosy-1lc4v.mongodb.net/test?retryWrites=true&w=majority',
+{ 
+    useNewUrlParser: true,
+     useUnifiedTopology: true 
+});
+
 server.use(express.json());
-
 server.use(routes);
-
 server.listen(3333);
