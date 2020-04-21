d7e716ee2d15ee5f7f222691fe7bf202bf3b92a1
ricardozv <ricardosilvanet03@gmail.com>
2020-04-20 22:57:32 -0300

    upload

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,11 @@
 const express = require('express');
-
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 
-routes.get('/',(req, res) => {
-    return res.json({ message: `fala meu compas ${req.query.name}` });
-});
 
-routes.post('/devs', (req, res)=>{
-    
-    return res.json(req.body);
-})
-
+routes.post('/sessions', upload.single('image'), SessionController.store);
 module.exports = routes;
